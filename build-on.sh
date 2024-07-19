7530e896324728e5f997a45047f4bb6d325ea9d1
Bruno Haible <bruno@clisp.org>
2024-07-19 02:11:17 +0200

    Work on check-openbsd.

--- build-on.sh
+++ build-on.sh
@@ -69,7 +69,7 @@ if test -n "$install_optional_dependencies_command"; then
   cd build-full
 
   # Configure.
-  ../configure --config-cache $configure_options > log1 2>&1; rc=$?; cat log1; test $rc = 0 || exit 1
+  ../configure --config-cache --with-included-libunistring $configure_options > log1 2>&1; rc=$?; cat log1; test $rc = 0 || exit 1
 
   # Build.
   $make > log2 2>&1; rc=$?; cat log2; test $rc = 0 || exit 1
