34fa2a87288f6525514db3cca07071370f77ea2d
Bruno Haible <bruno@clisp.org>
2025-04-14 23:35:26 +0200

    Update from the newest template.

--- build-tarball.sh
+++ build-tarball.sh
@@ -25,7 +25,7 @@ set -e
 
 # Fetch sources (uses package 'git').
 git clone --depth 1 https://git.savannah.gnu.org/git/"$package".git
-git clone --depth 1 https://git.savannah.gnu.org/git/gnulib.git
+git clone --depth 1 "${gnulib_url}"
 
 # Apply patches.
 (cd "$package" && patch -p1 < ../patches/ubsan.diff)
