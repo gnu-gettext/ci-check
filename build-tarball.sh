670cf1292f0a409348e8c21f6f76e2a1a0768513
Bruno Haible <bruno@clisp.org>
2025-09-13 16:18:59 +0200

    Work on build-tarball: Refactor customization of git URLs.

--- build-tarball.sh
+++ build-tarball.sh
@@ -23,9 +23,11 @@ package="$1"
 
 set -e
 
+. ./init-git.sh
+
 # Fetch sources (uses package 'git').
-git clone --depth 1 https://https.git.savannah.gnu.org/git/"$package".git
-git clone --depth 1 "${gnulib_url}"
+git clone --depth 1 https://git.savannah.gnu.org/git/"$package".git
+git clone --depth 1 https://git.savannah.gnu.org/git/gnulib.git
 
 # Apply patches.
 (cd "$package" && patch -p1 < ../patches/ubsan.diff)
